ff7b1b78b913c58c389fb245102b979652c8ee01
Administrator <himanshu00qwerty@gmail.com>
2020-08-12 17:36:57 +0530

    Updates

---
+++ precache-manifest.a9d08e781eee2c0d33988ff92e552e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1326b9fd29de24a37a3299f80ae3bdb8",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "1e33ad233ccbcd055ba3",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/react/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e33ad233ccbcd055ba3",
+    "url": "/react/static/js/main.56ea63b3.chunk.js"
+  },
+  {
+    "revision": "e13f72c0a5b2117a7fa1",
+    "url": "/react/static/js/runtime-main.da8bee19.js"
+  }
+]);
